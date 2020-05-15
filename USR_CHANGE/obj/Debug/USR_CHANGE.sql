9edc29001da9a47de996c21345c17eec6a3ab13a
mkomaravolu <mkomaravolu@ciber.com>
2020-05-15 23:02:26 +0530

    Update T_USR  table

--- USR_CHANGE/obj/Debug/USR_CHANGE.sql
+++ USR_CHANGE/obj/Debug/USR_CHANGE.sql
@@ -1,7 +1,7 @@
 ﻿/*
     Target database:    USR_DEV
     Target instance:    CBR-LAP-DEL2715\SQLEXPRESS
-    Generated date:     2020-05-15T22:43:44Z
+    Generated date:     2020-05-15T23:01:55Z
     Generated on:       CBR-LAP-DEL2715
     Package version:    (undefined)
     Migration version:  (n/a)
